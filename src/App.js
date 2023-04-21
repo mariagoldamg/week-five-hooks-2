db0ad3d736f042b00f8962474a9d2c71dbc68e4a
mariagoldamg <mariamelnikova88@gmail.com>
2023-04-21 10:11:33 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App (){
+
+  const [coding, setCoding] = useState ('JavaScript'); 
+
+  const updateData =()=>{
+    
+    if (coding==='JavaScript'){
+      setCoding ('React')
+    }
+    
+ else {setCoding ('JavaScript')}
+  }
+
+  return(
+    <div>
+<h1>I Love {coding}</h1>
+<button onClick={updateData}>Click Here</button>
     </div>
-  );
+  )
 }
 
+
+/*function App() {
+
+  const [number, setNumber] = useState (100);
+
+  const changeNumber = () =>{
+    setNumber(number -5);
+  }
+
+  return (
+    <div >
+      <p>{number}</p>
+      <button onClick ={changeNumber}> Cick here</button>
+    </div>
+  );
+}*/
+
+/*class App extends Component{
+
+  state = {
+    number:100
+  }
+
+  render(){
+    return(
+      <div>
+        <p> {this.state.number}</p>
+        <button onClick= {() => this.setState({number: this.state.number-5})}> Click here! </button>
+      </div>
+    )
+  }
+}
+*/
 export default App;
